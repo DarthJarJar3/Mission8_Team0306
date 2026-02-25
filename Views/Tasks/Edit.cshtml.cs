548c514693e1111c1cf2e61540a67b75a39c54d2
Joshua Ashby <ashby10@byu.edu>
2026-02-25 14:41:03 -0700

    add file and edit file

---
+++ Views/Tasks/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mission8_Team0306.Views.Tasks
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
